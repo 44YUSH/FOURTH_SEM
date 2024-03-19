2ef0b9fd4484a61ca442e4f9970540398a011843
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-03-19 13:53:33 +0530

    Add files via upload

---
+++ CSW_2 Class_Lec/Error_Handling.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Error_Handling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int v1=5+4*3/2;
+		int v2=(5+4)*3/2;
+		int v3=(5+4)*(3/2);
+		int v4=(5+(4*3))/2;
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v3);
+		System.out.println(v4);
+		
+		int c;
+		for(c=1; c<10; c++)
+			System.out.println("Counter is "+c);
+	}
+
+}
