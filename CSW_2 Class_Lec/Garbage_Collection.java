6becb7b93abb6442664fb2e22c4bc35c66a838e0
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-03-20 10:36:57 +0530

    Create Garbage_Collection.java

---
+++ CSW_2 Class_Lec/Garbage_Collection.java
@@ -0,0 +1,33 @@
+class Employee{
+  private int ID;
+  private String name;
+  private int age;
+  private static int nextId = 1;
+  public Employee(String name, int age){
+    this.name=name;
+    this.age=age;
+    this.ID=nextId++;
+  }
+  public void show(){
+    system.out.println("Next Employee id will be ="+nextId);
+  }
+}
+class Garbage_Collection_1{
+  public static void main(String[]args){
+    Employee E = new Employee("Anisha", 20);
+    Employee F = new Employee("Sidlu", 40);
+    Employee G = new Employee("SubhaCR", 150);
+    E.show();
+    F.show();
+    G.show();
+    {
+      Employee X = new Employee("Subham", 21);
+      Employee E = new Employee("Aayush", 20);
+      X.show();
+      Y.show();
+      X.showNextId();
+      Y.showNextId();
+    }
+    E.showNextId();
+  }
+}
