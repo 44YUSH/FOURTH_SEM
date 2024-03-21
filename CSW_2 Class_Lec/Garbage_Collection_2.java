099a95f7e7c3873554b300f95ef481b602e7f1b5
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-03-21 11:54:20 +0530

    Add files via upload

---
+++ CSW_2 Class_Lec/Garbage_Collection_2.java
@@ -0,0 +1,54 @@
+class Emp{
+  private int ID;
+  private String name;
+  private int age;
+  private static int nextId = 1;
+  public Emp(String name, int age){
+    this.name=name;
+    this.age=age;
+    this.ID=nextId++;
+  }
+   public void show(){
+    System.out.println("( Id : "+ID+" ) ( Name : "+name+")  ( Age : "+age+" )");
+  }
+  public void showNextId(){
+    System.out.println("Next Employee id will be ="+nextId);
+  }
+  public void gap() {
+	  System.out.println();
+  }
+  protected void finalize() {
+	  --nextId;
+  }
+}
+class Garbage_Collection_2{
+  public static void main(String[]args){
+    Emp E = new Emp("Anisha ", 20);
+    E.show();
+    E.showNextId();
+    E.gap();
+    Emp F = new Emp("Sidlu  ", 40);
+    F.show();
+    F.showNextId();
+    F.gap();
+    Emp G = new Emp("Subha  ", 150);
+    G.show();
+    G.showNextId();
+    G.gap();
+    {
+      Emp X = new Emp("Subham ", 21);
+      X.show();
+      X.showNextId();
+      X.gap();
+      Emp Y = new Emp("Aayush ", 20);
+      Y.show();
+      Y.showNextId();
+      Y.gap();
+      
+      X=Y=null;
+      System.gc();
+      System.runFinalization();
+    }
+    E.showNextId();
+  }
+}
