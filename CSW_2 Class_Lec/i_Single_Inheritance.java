c03a1da952ebdb6498cb058fc42e0394a89a2aef
Aayush <aayushsith0@gmail.com>
2024-03-23 21:42:45 +0530

    push

---
+++ CSW_2 Class_Lec/i_Single_Inheritance.java
@@ -0,0 +1,22 @@
+
+class Animals {
+	
+	void eat(){
+		System.out.println("Eating...");
+	}
+}
+	class dog extends Animals{
+		public void bark() {
+			System.out.println("Baking...");
+	}
+}
+
+ public class i_Single_Inheritance{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dog obj=new dog();
+		obj.eat();
+		obj.bark();
+	}
+ }
+
