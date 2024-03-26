9696f7c20e500f089932b1fc225d427edf030ea7
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-03-26 07:52:36 +0000

    push

--- CSW_2 Class_Lec/Polymorphism_CompileTime.java
+++ CSW_2 Class_Lec/Polymorphism_CompileTime.java
@@ -6,9 +6,16 @@ class Multiply{
 		return a*b*c;
 	}
 }
-public class Compile_Time_Polymorphism {
+public class Polymorphism_CompileTime {
 	public static void main(String[]args) {
 		System.out.println(Multiply.mult(3, 5));
 		System.out.println(Multiply.mult(3, 5, 2));
 	}
 }
+
+/*
+OUTPUT
+
+15
+30
+*/
