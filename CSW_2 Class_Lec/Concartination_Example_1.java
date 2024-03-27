2129f0d37b51c1d1307800de594f92d112d3fc97
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-03-27 04:59:23 +0000

    push

---
+++ CSW_2 Class_Lec/Concartination_Example_1.java
@@ -0,0 +1,34 @@
+public class Concartination_Example_1 {
+    public static void main(String[] args) {
+        String s1, s2, s3;
+        s1="Hello";
+        s2="";
+        s3=s1.concat(s2);
+        System.out.println("Case 1 -- Concat");
+        if(s1==s3){
+            System.out.println("Both are same.");
+        }
+        else{
+            System.out.println("Not same.");
+        }
+        System.out.println();
+        System.out.println("Case 2 -- '+'");
+        String s4=s1+s2;
+        if(s1==s4){
+            System.out.println("Both are same.");
+        }
+        else{
+            System.out.println("Not same");
+        }
+    }
+}
+
+/*
+OUTPUT
+
+Case 1 -- Concat
+Both are same.
+
+Case 2 -- '+'
+Not same
+*/
