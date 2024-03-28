663fb4564ea01e53ea40440bd742977efbccb2ef
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-03-28 06:31:47 +0000

    push

--- CSW_2 Class_Lec/String_Splitting.java
+++ CSW_2 Class_Lec/String_Splitting.java
@@ -2,11 +2,19 @@
 
 public class String_Splitting {
     public static void main(String[] args) {
-        String my_str="ITER :A SOA University";
-        String delimiter=":";
-        String [] splits=my_str.split(delimiter);
+        String str1="ITER :A SOA University";
+        String delimiter1=":";
+        String [] splits=str1.split(delimiter1);
         for(String a : splits){
             System.out.println(a);
+        }
+        System.out.println(".................");
+
+        String srt2="Hello @How @Are @You";
+        String delimiter2="@";
+        String [] split=srt2.split(delimiter2);
+        for(String b : split){
+            System.out.println(b);
         }
     }
 }
@@ -16,5 +24,9 @@ OUTPUT
 
 ITER 
 A SOA University
-
+.................
+Hello 
+How 
+Are 
+You
 */
