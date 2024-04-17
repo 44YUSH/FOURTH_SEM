6f06e3cb50c1039478aa8f598baadb9369a6de5e
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-04-17 21:23:13 +0530

    Update i_Single_Inheritance.java

--- CSW_2/Class_Lectures/i_Single_Inheritance.java
+++ CSW_2/Class_Lectures/i_Single_Inheritance.java
@@ -5,13 +5,13 @@ class Animals {
 		System.out.println("Eating...");
 	}
 }
-	class dog extends Animals{
-		public void bark() {
-			System.out.println("Baking...");
+class dog extends Animals{
+	public void bark() {
+		System.out.println("Baking...");
 	}
 }
 
- public class i_Single_Inheritance{
+public class i_Single_Inheritance{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		dog obj=new dog();
