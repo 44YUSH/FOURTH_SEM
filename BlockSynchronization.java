4044bbcc0a45b982b712c64e8c64ef13be96add5
A.a.y.u.s.h <151053890+44YUSH@users.noreply.github.com>
2024-05-08 04:32:19 +0000

    (~_~)

---
+++ BlockSynchronization.java
@@ -0,0 +1,44 @@
+public class BlockSynchronization{
+    public static void main(String[]args){
+        Calculator calculator=new Calculator();
+        WorkerThread1 t1=new WorkerThread1(calculator);
+        WorkerThread2 t2=new WorkerThread2(calculator);
+        t1.start();
+        t2.start();
+    }
+}
+class Calculator{
+    void multiplicationTable(int n){
+        synchronized(this){
+            for(int i=1; i<=10; i++){
+                System.out.println(Thread.currentThread().getName()+" : "+n*i);
+                try{
+                    Thread.sleep(400);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
+class WorkerThread1 extends Thread{
+    Calculator t;
+    WorkerThread1(Calculator t){
+        this.t=t;
+        this.setName("Worker Thread 1");
+    }
+    public void run(){
+        t.multiplicationTable(3);
+    }
+}
+class WorkerThread2 extends Thread{
+    Calculator t;
+    WorkerThread2(Calculator t){
+        this.t=t;
+        this.setName("Worker Thread 2");
+    }
+    public void run(){
+        t.multiplicationTable(40);
+    }
+}
